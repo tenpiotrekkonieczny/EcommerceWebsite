db0231d68b86ac914d279a2cc0bb3bb3c7ba5cb3
Piotr Konieczny <piotrekprogramista@gmail.com>
2023-05-17 13:58:53 +0200

    copy menu for mobile

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // copy menu for mobile
 
 function copyMenu {
+// copy inside .dpt-cat to .departaments
+let dptCategory = document.querySelector('.dpt-cat');
+let dptPlace = document.querySelector('.departaments');
+dptPlace.innerHTML = dptCategory.innerHTML;
 
+
+// copy inside nav to nav
+let mainNav = document.querySelector('.header-nav nav');
+let navPlace = document.querySelector('off-canvas nav');
+navPlace.innerHTML = mainNav.innerHTML;
+
+// copy .header-top .wrapper to .thetop-nav
+let topNav = document.querySelector('.header-top');
+let topPlace = document.querySelector('.off-canvas .thetop-nav')
+topPlace.innerHTML = topNav.innerHTML;
 }
 
 copyMenu();
